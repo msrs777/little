f68495525df0ac613025b341c10834057514d482
ms <mswain.db@gmail.com>
2026-09-09 09:13:34 +0000

    Add teddy product photo

---
+++ js/products.js
@@ -0,0 +1,16 @@
+const products = [
+    {
+        id: 1,
+        name: "Teddy Bear",
+        price: 1500,
+        description: "Beautiful soft teddy bear.",
+        image: "images/product-001/main.jpg",
+
+        gallery: [
+            "images/product-001/main.jpg",
+            "images/product-001/angle-1.jpg",
+            "images/product-001/angle-2.jpg",
+            "images/product-001/angle-3.jpg"
+        ]
+    }
+];
